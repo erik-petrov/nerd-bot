49910f576e04f2f740214184c5eeaaa825a767c6
KOTEMAN123 <koteman123@hotmail.com>
2022-09-18 14:59:11 +0300

    First commit

---
+++ bot.py
@@ -0,0 +1,20 @@
+import os
+import discord
+
+TOKEN = os.getenv('NERD_BOT')
+nerds = [465770139501985812, 291477074236014593]
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_message(msg):
+    if msg.author == client.user:
+        return
+    if msg.author.id in nerds:
+        await msg.add_reaction("🤓")
+    
+
+client.run(TOKEN)
