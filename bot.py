58cd4e974b6b8556751a7b661fefbb5e8f7698cf
KOTEMAN123 <koteman123@hotmail.com>
2022-09-18 15:46:44 +0300

    add commands, make extreme mode

--- bot.py
+++ bot.py
@@ -1,22 +1,57 @@
 import os
+from pydoc import describe
 import discord
+from discord.ext import commands
+import pymongo
 
-TOKEN = os.getenv('NERD_BOT')
+TOKEN = str(os.getenv('NERD_BOT'))
 password = str(os.environ.get("password"))
 
-nerds = [465770139501985812, 291477074236014593]
+admins = [291477074236014593]
+client = pymongo.MongoClient("mongodb+srv://koteman123:"+password+"@cluster0.83jlpjn.mongodb.net/?retryWrites=true&w=majority")
+mydb = client["nerd-bot"]
+nerds = mydb["nerds"]
+extreme = False
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='/')
 
-@client.event
+@bot.event
 async def on_message(msg):
     if msg.author == client.user:
         return
-    if msg.author.id in nerds:
+    if nerds.find_one({"discordID": msg.author.id}) != None:
+        if(extreme): await msg.reply("🤓")
         await msg.add_reaction("🤓")
+#You must have access to the message_content intent for the commands extension to function. This must be set both in the developer portal and within your code.
+@bot.command(name="addNerd", description="Adds a nerd. Use the nerd's ID.")
+async def addNerd(ctx, nerdId):
+    if not nerdId.isnumeric():
+        return
+    if ctx.author.id not in admins:
+        return
+    if nerds.find_one({"discordID": ctx.author.id}) != None:
+        if x.discordID == nerdId:
+            ctx.send("already in db")
+            return
+    newNerd = {
+        "discordID": nerdId
+    }
+    nerds.insert_one(newNerd)
+    ctx.send("successfully added")
+        
+@bot.command(name="extreme", description="Chaos.")
+async def extreme(ctx):
+    if ctx.author.id not in admins:
+        return
+    extreme = not extreme
+    if extreme:
+        ctx.send("Chaos mode active.")
+    else:
+        ctx.send("Chaos averted.")
     
 
-client.run(TOKEN)
+bot.run(TOKEN)
